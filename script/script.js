f1674e191ac87fa412a7b1dadf0c8e44eedc72b2
GalaletsangM <preciousmafoko@gmail.com>
2024-03-17 06:22:40 -0500

    removed the about.html and work.html page
    Created a one page website. Did the styling and basic javascript

---
+++ script/script.js
@@ -0,0 +1,25 @@
+function toggleMenu() {
+  const menu = document.querySelector(".manu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
+
+function sendEmail() {
+  Email.send({
+    Host: "smtp.gmail.com",
+    Username: "preciousmafoko@gmail.com",
+    Password: "@A.b.c.123",
+    To: "preciousmafoko@gmail.com",
+    From: document.getElementById("email").value,
+    Subject: "New Contact form enquiry",
+    Body:
+      "Name: " +
+      document.getElementById("name").value +
+      "<br> Email: " +
+      document.getElementById("email".value) +
+      "<br> Phone number: " +
+      document.getElementById("phone").value +
+      "<br> Message: " + document.getElementById("message").value,
+  }).then((message) => alert("Message sent succesfully!"));
+}
